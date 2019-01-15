5736da2962fd83b4d9c355fce93ded5ca09923bb
hongkai <952817116@qq.com>
2019-01-15 11:02:19 +0800

    bug作废

--- 轮播初学/lunbo.js
+++ 轮播初学/lunbo.js
@@ -1,9 +1,7 @@
-$('.images > img:nth-child(1)').addClass('current')
-$('.images > img:nth-child(2)').addClass('ready')
-$('.images > img:nth-child(3)').addClass('ready')
-let n = 1
+let n
+y()
 
-setInterval(() => {
+let timer = setInterval(() => {
     $(`.images > img:nth-child(${x(n)})`).removeClass('current').addClass('leave')
         .one('transitionend', (x) => {
             $(x.currentTarget).removeClass('leave').addClass('ready')
@@ -12,12 +10,36 @@ setInterval(() => {
     n += 1
 }, 3000)
 
+
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        window.clearInterval(timer)
+    } else {
+        timer = setInterval(() => {
+            $(`.images > img:nth-child(${x(n)})`).removeClass('current').addClass('leave')
+                .one('transitionend', (x) => {
+                    $(x.currentTarget).removeClass('leave').addClass('ready')
+                })
+            $(`.images > img:nth-child(${x(n + 1)})`).removeClass('ready').addClass('current')
+            n += 1
+        }, 3000)
+    }
+})
+
+
+
 function x(n) {
     if (n > 3) {
         n = n % 3
-        if(n===0){
+        if (n === 0) {
             n = 3
         }
     }
     return n
+}
+
+function y() {
+    n = 1
+    $(`.images > img:nth-child(${n})`).addClass('current')
+        .siblings().addClass('ready')
 }
