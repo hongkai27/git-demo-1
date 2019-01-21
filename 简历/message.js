a076d553617ff2a83132a8942706b40e36cd0e25
hongkai <952817116@qq.com>
2019-01-21 22:43:37 +0800

    简历留言

---
+++ 简历/message.js
@@ -0,0 +1,51 @@
+var APP_ID = 'FMSL4j7PnQPfIBaNQhVC9mw1-gzGzoHsz';
+var APP_KEY = 'Rzj5LwGFvKjkTpzTQPakSkeB';
+
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+});
+
+
+let myform = document.querySelector('#postMessage')
+myform.addEventListener('submit', function (e) {
+  e.preventDefault();//用户点击了表单
+  var content = myform.querySelector('input[name=content]').value;
+  var Message = AV.Object.extend('Message');
+  var message = new Message();
+  message.save({//就把输入的信息存到服务器中，
+    'content': content//用户输入的内容
+  }).then(function () {
+    window.location.reload()//成功了就帮助用户自动刷新
+  })
+})
+
+/*var TestObject = AV.Object.extend('TestObject');//新建一个表单,表单名字是TestObject
+var testObject = new TestObject();
+testObject.save({//在表中新建一行数据，内容为HELLO WORLD ，保存成功，就运行alert
+  words: 'Hello World!'
+}).then(function(object) {
+  alert('LeanCloud Rocks!');
+})
+*/
+
+var query = new AV.Query('Message');//表单在上一步已经建好了
+query.find()//执行Message函数，代码框架照抄
+  .then(
+    function (messages) {//这个messages是我们获取的信息，一个数组，系统传给我们的，不是我们设置的参数
+      let array = messages.map((item)=> item.attributes)//map一下，返回item每一项的attributes，形成一个新的数组
+      array.forEach((item)=>{                 //遍历一下数组里面的每一个item
+        let li = document.createElement('li')
+        li.innerText = item.content
+        let messageList = document.querySelector('#messageList')
+        messageList.append(li)//append和appendChild现在都可用
+      })
+    },
+    function (error) {
+      alert('提交失败')
+      // 异常处理
+    })
+    .then(//上一个then有报错时执行这里的函数
+      ()=>{},
+      (error)=>[]
+    )
--- 简历/server.js
+++ 简历/server.js
@@ -1,41 +1,103 @@
-var http = require('http')
-var fs = require('fs')
-var url = require('url')
-var port = process.argv[2]
+var mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  loop: true,
 
-if(!port){
-  console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
-  process.exit(1)
-}
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
 
-var server = http.createServer(function(request, response){
-  var parsedUrl = url.parse(request.url, true)
-  var pathWithQuery = request.url 
-  var queryString = ''
-  if(pathWithQuery.indexOf('?') >= 0){ queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
-  var path = parsedUrl.pathname
-  var query = parsedUrl.query
-  var method = request.method
-
-  /******** 从这里开始看，上面不要看 ************/
-
-  console.log('方方说：含查询字符串的路径\n' + pathWithQuery)
-
-  if(path === '/'){
-    response.statusCode = 200
-    response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write('哈哈哈')
-    response.end()
-  }else{
-    response.statusCode = 404
-    response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write('呜呜呜')
-    response.end()
-  }
-
-  /******** 代码结束，下面不要看 ************/
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 })
 
-server.listen(port)
-console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
+ //将页面初始的第一个元素设置为原来位置，其他元素有动画
+ let specialTags = document.querySelectorAll('[data-x]')
+ for (let i = 0; i < specialTags.length; i++) {
+   specialTags[i].classList.add('offset')
+ }
 
+ //作品展示按钮点击后滑条滚动
+ portfolio1.onclick = function () {
+   portfoliobar.className = 'bar state-1'
+ }
+ portfolio2.onclick = function () {
+   portfoliobar.className = 'bar state-2'
+ }
+ portfolio3.onclick = function () {
+   portfoliobar.className = 'bar state-3'
+ }
+
+ //页面滑动后导航栏定位在顶部
+ setTimeout(function () {
+   donghua()
+ }, 500)
+ window.onscroll = function (xxx) {
+   if (window.scrollY > 0) {
+     topNavBar.classList.add('sticky')
+   } else {
+     topNavBar.classList.remove('sticky')
+   }
+   donghua()
+ }
+ function donghua() {
+   //滚动页面后对应的导航栏元素标签到中间后自动高亮
+   let specialTags = document.querySelectorAll('[data-x]')
+   let minindex = 0;
+   for (let i = 1; i < specialTags.length; i++) {
+     if (Math.abs(specialTags[i].offsetTop - window.scrollY) < Math.abs(specialTags[minindex].offsetTop - scrollY)) {
+       minindex = i;
+     }
+   }
+   //minindex就是离窗口顶端最近的元素
+   specialTags[minindex].classList.remove('offset')
+   let id = specialTags[minindex].id
+   let a = document.querySelector('a[href="#' + id + '"]');
+   let li = a.parentNode;
+   let brothersAndme = li.parentNode.children;
+   for (let i = 0; i < brothersAndme.length; i++) {
+     brothersAndme[i].classList.remove('light');
+   }
+   li.classList.add('light');
+ }
+
+ //鼠标悬浮导航栏标签后滑动条高亮
+ let liTags = document.querySelectorAll('nav.menu >ul >li');
+ for (let i = 0; i < liTags.length; i++) {
+   liTags[i].onmouseenter = function (x) {
+     x.currentTarget.classList.add('active');
+   }
+   liTags[i].onmouseleave = function (x) {
+     x.currentTarget.classList.remove('active');
+   }
+ }
+
+ //点击导航栏标签后滑行到文档对应的元素标签
+ function animate(time) {
+   requestAnimationFrame(animate);
+   TWEEN.update(time);
+ }
+ requestAnimationFrame(animate);
+ let aTags = document.querySelectorAll('nav.menu >ul >li >a');
+ for (let i = 0; i < aTags.length; i++) {
+   aTags[i].onclick = function (x) {
+     x.preventDefault();
+     let a = x.currentTarget;
+     let href = a.getAttribute('href');
+     let element = document.querySelector(href);
+     let top = element.offsetTop;
+     let currentTop = window.scrollY;
+     let targetTop = top - 76;
+     var coords = { y: currentTop };
+     var tween = new TWEEN.Tween(coords)
+       .to({ y: targetTop }, 500)
+       .easing(TWEEN.Easing.Quadratic.InOut)
+       .onUpdate(function () {
+         window.scrollTo(0, coords.y)
+       })
+       .start();
+   }
+ }
