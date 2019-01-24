2e3db079dadada0e63cf2d4a1f2a965f3233a3a1
hongkai <952817116@qq.com>
2019-01-24 23:05:45 +0800

    无刷新留言

--- 简历/message.js
+++ 简历/message.js
@@ -2,7 +2,7 @@
   var view = document.querySelector('.message')
   var model = {
     //获取保存数据
-    fetch: function (name,content) {
+    fetch: function (name, content) {
       var Message = AV.Object.extend('Message');  //新建名为Message的表单
       var message = new Message();  //在表中新建一行数据
       return message.save({//是一个promise对象,就把输入的信息存到服务器中
@@ -38,35 +38,36 @@
         e.preventDefault();//用户点击表单后阻止执行程序
         var name = myform.querySelector('input[name=name]').value;
         var content = myform.querySelector('input[name=content]').value;
-        model.fetch(name,content).then(function (x) {
+        model.fetch(name, content).then(function (x) {  //不刷新页面，提交时直接添加留言
           let li = document.createElement('li')//注意：模板字符串中的${}表示占位符，不是JQ中的$
-          li.innerText = `${x.attributes.name}:${x.attributes.content}`//不刷新页面，直接添加留言
+          li.innerText = `${x.attributes.name}:${x.attributes.content}`
           let messageList = document.querySelector('#messageList')
           messageList.append(li)//append和appendChild现在都可用
-          myform.querySelector('input[name=content]').value = ''   //输入内容提交后清空
+          myform.querySelector('input[name=content]').value = '' //输入内容提交后清空
           myform.querySelector('input[name=name]').value = ''
-        })//在这里把内容变成li是因为要以这种形式存储进去，下面提取的时候重复操作就可以拿到信息
+        })
       })
     },
     showMessages: function () {
-      //展示留言
+      //获取留言。。。。作用是页面第一次加载时批量获取数据
       model.show().then(
-          function (messages) {//这个messages是我们获取的信息，一个数组，系统传给我们的，不是我们设置的参数
-            let array = messages.map((item) => item.attributes)//map一下，返回item每一项的attributes，形成一个新的数组
-            array.forEach(
-              (item) => {                 //遍历一下数组里面的每一个item
-                let li = document.createElement('li')
-                li.innerText = `${item.name}:${item.content}`
-                let messageList = document.querySelector('#messageList')
-                messageList.append(li)//append和appendChild现在都可用
-              }
-            )
-          },
-          function (error) {
-            alert('提交失败')
-            // 异常处理
-          })
+        function (messages) {//这个messages是我们获取的信息，一个数组，系统传给我们的，不是我们设置的参数
+          let array = messages.map((item) => item.attributes)//map一下，返回item每一项的attributes，形成一个新的数组
+          array.forEach(
+            (item) => {                 //遍历一下数组里面的每一个item
+              let li = document.createElement('li')
+              li.innerText = `${item.name}:${item.content}`
+              let messageList = document.querySelector('#messageList')
+              messageList.append(li)//append和appendChild现在都可用
+            }
+          )
+        },
+        function (error) {
+          alert('提交失败')
+          // 异常处理
+        })
     }
+
   }
   controller.init(view)
 }.call()
